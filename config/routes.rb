50b5cdcae029ae0f4923bba4cc8bae68179a5755
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-03-19 21:08:56 +0900

    モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :user_addresses
   devise_for :users
   root to: 'top_pages#index'
-  resources :items only: 
   resources :items, only: [:new, :create]
 
 end
