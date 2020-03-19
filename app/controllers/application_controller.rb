379365759ecfbbec01f2bc6db92cb376b19e69a3
kazuki-afb <kauzki_afb@yahoo.co.jp>
2020-03-19 18:52:56 +0900

    devise_user_setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:name,:first_name_kana,:name_kana,:birthday,:phone_number])
 end
