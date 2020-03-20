7a2b9636d6aa6bb01d41f4b491bf0f7f98e9910b
kazuki-afb <kauzki_afb@yahoo.co.jp>
2020-03-20 19:16:46 +0900

    user_addバリテーション

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,18 @@ ActiveRecord::Schema.define(version: 2020_03_20_060238) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "content", null: false
+    t.string "state", null: false
+    t.integer "postage", null: false
+    t.integer "shipping_date", null: false
+    t.integer "price", null: false
+    t.string "region", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_destinaition_name", null: false
     t.string "destinaition_name", null: false
@@ -54,6 +66,24 @@ ActiveRecord::Schema.define(version: 2020_03_20_060238) do
     t.integer "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "name", null: false
+    t.string "first_name_kana", null: false
+    t.string "name_kana", null: false
+    t.date "birthday", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
