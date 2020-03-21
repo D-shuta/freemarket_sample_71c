316d071053f9a88b83f2144e996f5e9fdb75355e
D-shuta <crime-of-visualshock-you-are-x@softbank.ne.jp>
2020-03-21 18:01:02 +0900

    修正２

--- db/schema.rb
+++ db/schema.rb
@@ -43,15 +43,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_112102) do
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "content", null: false
-    t.string "state", null: false
-    t.integer "postage", null: false
-    t.integer "shipping_date", null: false
-    t.integer "price", null: false
-  end
-
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_destinaition_name", null: false
     t.string "destinaition_name", null: false
