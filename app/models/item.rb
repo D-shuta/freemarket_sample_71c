fe0ebe47027bdc5dbc3e476ac1fbb367ded372fb
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-03-21 10:49:37 +0900

    発送の日付セレクト型に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
   
   validates :name, presence: true, length: { maximum: 40 }
   validates :content, presence: true, length: { maximum: 1000 }
