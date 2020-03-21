fa680acedadfd680594f3c5014a26d2ddb0796d4
D-shuta <crime-of-visualshock-you-are-x@softbank.ne.jp>
2020-03-21 17:59:03 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_112102) do
     t.integer "postage", null: false
     t.integer "shipping_date", null: false
     t.integer "price", null: false
-    t.string "region", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
