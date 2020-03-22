7bb4cf70fe75f400e9a4eaaf18079cc99a7720a3
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-03-22 23:42:02 +0900

    「購入者」「出品者」定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping
+  belongs_to_active_hash :shipping  
+  has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+  belongs_to :saler, class_name: "User"
+  belongs_to :buyer, class_name: "User"
   
   validates :name, presence: true, length: { maximum: 40 }
   validates :content, presence: true, length: { maximum: 1000 }
@@ -10,7 +14,4 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :price, presence: true
   validates :images, presence: true
-  
-  has_many :images
-  accepts_nested_attributes_for :images, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,8 @@ class User < ApplicationRecord
   validates :name_kana, :first_name_kana, presence: true
     # 生年月日は必須
   validates :birthday, presence: true
+
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
 end
