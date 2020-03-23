37f9931f2ef8d4ecb93830862521ea19e3cd216f
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-03-23 19:19:06 +0900

    単体テスト成功

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,5 +8,53 @@ require 'rails_helper'
         expect(item).to be_valid
       end
 
+      it "商品名が空欄では出品できない" do
+        item = build(:item, name: nil)
+        item.valid?
+        expect(item.errors[:name]).to include("can't be blank")
+      end
+
+      it "商品説明が空欄では出品できない" do
+        item = build(:item, content: nil)
+        item.valid?
+        expect(item.errors[:content]).to include("can't be blank")
+      end
+
+      it "商品状態が空欄では出品できない" do
+        item = build(:item, state: nil)
+        item.valid?
+        expect(item.errors[:state]).to include("can't be blank")
+      end
+
+      it "配送日数が空欄では出品できない" do
+        item = build(:item, shipping_id: nil)
+        item.valid?
+        expect(item.errors[:shipping_id]).to include("can't be blank")
+      end
+
+      it "配送元が空欄では出品できない" do
+        item = build(:item, prefecture_id: nil)
+        item.valid?
+        expect(item.errors[:prefecture_id]).to include("can't be blank")
+      end
+
+      it "金額が空欄では出品できない" do
+        item = build(:item, price: nil)
+        item.valid?
+        expect(item.errors[:price]).to include("can't be blank")
+      end
+
+      it "seller_idが空欄では出品できない" do
+        item = build(:item, seller_id: nil)
+        item.valid?
+        expect(item.errors[:seller_id]).to include("can't be blank")
+      end
+
+      it "category_id が空欄では出品できない" do
+        item = build(:item, postage: nil)
+        item.valid?
+        expect(item.errors[:postage]).to include("can't be blank")
+      end
+
     end
   end
