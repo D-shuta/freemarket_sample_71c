fe3d0015a053554e8056c4dd26be271ff4db2b85
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-03-23 19:49:42 +0900

    リファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,33 +11,12 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", optional: true
   
 
-  # - 商品名が必須
   validates :name, presence: true, length: { maximum: 40 }
-
-  # - 商品の説明が必須
   validates :content, presence: true, length: { maximum: 1000 }
 
-  # - 商品の状態についての情報が必須
-  validates :state, presence: true
 
-  # - 発送までの日数についての情報が必須
-  validates :shipping_id, presence: true
+  validates :postage, :category_id, :seller_id, 
+  :images, :price, :prefecture_id, :shipping_id, :state,
+   presence: true
 
-  # - 発送元の地域についての情報が必須
-  validates :prefecture_id, presence: true
-
-  # - 価格についての情報が必須
-  validates :price, presence: true
-
-  # - 画像は1枚以上必須
-  validates :images, presence: true
-
-  # - 出品者情報必須
-  validates :seller_id, presence: true
-
-  # - カテゴリーの情報が必須
-  validates :category_id, presence: true
-
-  # - 配送料の負担についての情報が必須
-  validates :postage, presence: true
 end
