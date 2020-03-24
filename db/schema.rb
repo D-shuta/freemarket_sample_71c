e24a6cdfddffdad65586e654260b9ab676b04090
D-shuta <crime-of-visualshock-you-are-x@softbank.ne.jp>
2020-03-24 16:04:12 +0900

    コンフリクト対策

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-   ActiveRecord::Schema.define(version: 2020_03_23_023934) do
-   ActiveRecord::Schema.define(version: 2020_03_23_063858) do
+ActiveRecord::Schema.define(version: 2020_03_23_023934) do
+ActiveRecord::Schema.define(version: 2020_03_23_063858) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
