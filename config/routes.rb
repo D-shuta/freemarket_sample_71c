854d5041493c061972d0790f615c359aeb56575b
D-shuta <crime-of-visualshock-you-are-x@softbank.ne.jp>
2020-03-24 16:00:26 +0900

    コンフリクト対策

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,4 @@ Rails.application.routes.draw do
       get  'get_category_grandchildren', defaults: { format: 'json' }
     end
   end
-  resources :card, only: [:new, :show] do
-    collection do
-      post 'show', to: 'card#show'
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
-    end
-  end
-
 end
