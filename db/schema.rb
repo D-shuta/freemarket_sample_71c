701092af797a987a451522fd6016cbfd21b0c5d4
KouichiItagaki <xxx.sms.xxx501@gmail.com>
2020-04-01 15:26:20 +0900

    FIX

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_070241) do
+ActiveRecord::Schema.define(version: 2020_04_01_062433) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -74,9 +74,11 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
     t.string "address", null: false
     t.string "building_name"
     t.bigint "phone_number"
+    t.bigint "users_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["users_id"], name: "index_user_addresses_on_users_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -98,4 +100,5 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
   end
 
   add_foreign_key "images", "items"
+  add_foreign_key "user_addresses", "users", column: "users_id"
 end
