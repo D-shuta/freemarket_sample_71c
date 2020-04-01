a44109274fc3911764afe19e155b4376b8b0307e
hosa-ichi <taichi_paj@yahoo.co.jp>
2020-04-01 12:27:44 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -56,6 +57,12 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
     t.integer "state_id"
   end
 
+  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "item_id"
+  end
+
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_destinaition_name", null: false
     t.string "destinaition_name", null: false
@@ -67,11 +74,9 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
     t.string "address", null: false
     t.string "building_name"
     t.bigint "phone_number"
-    t.bigint "users_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.index ["users_id"], name: "index_user_addresses_on_users_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -93,5 +98,4 @@ ActiveRecord::Schema.define(version: 2020_03_30_070241) do
   end
 
   add_foreign_key "images", "items"
-  add_foreign_key "user_addresses", "users", column: "users_id"
 end
